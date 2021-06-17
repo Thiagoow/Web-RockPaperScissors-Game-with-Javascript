c2abb134a929bdcc3da8149046b19d3b188e6539
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-06-17 19:13:04 -0300

    No 1:15 do vídeo 😍🤩😎😎

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,8 @@
 //SEMPRE TESTAMOS USANDO O console.log() NA VAR OU FUNÇÃO❗❗
 
 //Essas serão as vars para contar os pontos:
-const userScore = 0;
-const compScore = 0;
+let userScore = 0;
+let compScore = 0;
 
 //Selecionando as divs do HTML para exibir os pontos contados:
 const userScoreSpan = document.getElementById("userScore");
@@ -10,7 +10,8 @@ const compScoreSpan = document.getElementById("computerScore");
 
 //
 const scoreBoardDiv = document.querySelector(".scoreBoard");
-const resultDiv = document.querySelector(".result");
+const resultP = document.querySelector(".result p");
+const resultB = document.querySelector(".result b");
 
 /* Selecionando as divs de escolha de jogo do usuário 
 para verificar qual escolha ele fez: */
@@ -30,13 +31,6 @@ function computerChoice() {
   return choices[randomNum];
 }
 
-//
-function game(userChoice) {
-  const cpuMove = computerChoice();
-  console.log("PC -> " + cpuMove);
-  console.log("User -> " + userChoice);
-}
-
 //Verifica qual escolha o usuário fez:
 function clicked() {
   rockDiv.addEventListener("click", function () {
@@ -52,3 +46,72 @@ function clicked() {
   });
 }
 clicked();
+
+/*Executa as funções de vitória, derrota ou 
+empate de acordo com as escolhas do pc e do user: */
+function game(userChoice) {
+  const cpuMove = computerChoice();
+  switch (userChoice + cpuMove) {
+    case "rp":
+    case "pr":
+    case "sp":
+      resWin(userChoice, cpuMove);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      resLose(userChoice, cpuMove);
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      resDraw(userChoice, cpuMove);
+      break;
+  }
+}
+
+//Muda as letras "r, p, s" pra "Pedra, Papel, Tesoura"
+function convert2Word(letter) {
+  if (letter === "r") return "✊";
+  if (letter === "p") return "📃";
+  return "✂️";
+}
+
+//Se o usuário ganhar:
+function resWin(user, comp) {
+  //Soma +1 ponto pro user:
+  userScore++;
+  //Exibe na div do html os pontos do usuário:
+  userScoreSpan.innerHTML = userScore;
+  //Atualiza o HTML com os res:
+  resultP.innerHTML = `${convert2Word(user)} ganha de ${convert2Word(
+    comp
+  )}! Você`;
+  resultB.innerHTML = " VENCEU! 🤩😎";
+  //Deixa a cor da tag b:
+  resultB.style.color = "rgb(9, 255, 0)";
+}
+
+//Se o usuário perder:
+function resLose(user, comp) {
+  //Soma +1 ponto pro user:
+  compScore++;
+  //Exibe na div do html os pontos da CPU:
+  compScoreSpan.innerHTML = compScore;
+  //Atualiza o HTML com os res:
+  resultP.innerHTML = `${convert2Word(user)} ganha de ${convert2Word(
+    comp
+  )}! Você`;
+  resultB.innerHTML = " PERDEU! 🥺😥";
+  //Deixa a cor da tag b:
+  resultB.style.color = "rgb(255, 0, 0)";
+}
+
+//Se der empate:
+function resDraw(user, comp) {
+  //Atualiza o HTML com os res:
+  resultP.innerHTML = `${convert2Word(user)} = ${convert2Word(comp)}! Vocês`;
+  resultB.innerHTML = " EMPATARAM 😱 ";
+  //Deixa a cor da tag b:
+  resultB.style.color = "rgb(0, 17, 255)";
+}
